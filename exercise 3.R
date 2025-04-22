83f1ed2383bfd8bf0548e692b2cede1c65eb7692
philsperling <47870338+philsperling@users.noreply.github.com>
2025-04-22 15:53:43 +0200

    updated exercise 3

---
+++ exercise 3.R
@@ -0,0 +1,10 @@
+### Hypothesis test
+#We will use the lung dataset from the survival package (remember to install the package)
+
+# Task 1: explore the new dataset
+
+# Task 2: Is there a difference in age between dead and alive patients? (use the variable status to determin group)
+
+# Task 3: is there a difference in median survival time between male and female? 
+
+# Task 4: Is there a linear relationship between age and ecog score? 
